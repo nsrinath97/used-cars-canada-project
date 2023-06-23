8d7738588d05077b2496f9ccac61420e088ce23e
Srinath Nanduri <67294707+nsrinath97@users.noreply.github.com>
2023-06-23 19:34:21 +0000

    updated mongodb code, added sample post to test

--- export_to_mongodb.py
+++ export_to_mongodb.py
@@ -1,17 +1,31 @@
 from build_database import build_db_content
-import pymongo
-from pymongo import MongoClient
 import os
+from sample_posting import sample_post
+from pymongo.mongo_client import MongoClient
+from pymongo.server_api import ServerApi
+
+
 
 MONGO_DB_PASS = os.environ['MONGO_DB_PASS']
 
-cluster = MongoClient('mongodb+srv://nsrinath97:'+ MONGO_DB_PASS +'@used-cars-vancouver.2tpj6or.mongodb.net/?retryWrites=true&w=majority')
+uri = "mongodb+srv://nsrinath97:" + MONGO_DB_PASS + "@used-cars-vancouver.2tpj6or.mongodb.net/?retryWrites=true&w=majority"
+# Create a new client and connect to the server
+client = MongoClient(uri, server_api=ServerApi('1'))
+# Send a ping to confirm a successful connection
+try:
+    client.admin.command('ping')
+    print("Pinged your deployment. You successfully connected to MongoDB!")
+except Exception as e:
+    print(e)
 
-db = cluster['used_cars']
+
+db = client['used_cars']
 collection = db['posting_details']
 
-used_cars_database = build_db_content()
+# used_cars_database = build_db_content()
+# collection.insert_many(used_cars_database)
 
-collection.insert_many(used_cars_database)
+used_cars_database = sample_post
+collection.insert_one(used_cars_database)
 
-cluster.close()
+client.close()
---
+++ sample_posting.py
@@ -0,0 +1,17 @@
+sample_post = {
+ 'Vehicle Name': '2012 Honda Civic Sdn',
+ 'vehicle price': '$11,500',
+ 'city': 'Vancouver',
+ 'VIN': '2HGFB2F42CH008922',
+ 'condition': 'excellent',
+ 'cylinders': '4 cylinders',
+ 'drive': 'fwd',
+ 'fuel': 'gas',
+ 'odometer': '155000',
+ 'paint color': 'black',
+ 'size': 'compact',
+ 'title status': 'clean',
+ 'transmission': 'automatic',
+ 'type': 'sedan',
+ 'post link': 'https://vancouver.craigslist.org/van/ctd/d/2012-honda-civic-4dr-auto-155km-local-c/7635159524.html',
+ 'description': '  QR Code Link to This Post   2012 *** Honda Civic Sdn 4dr Auto LX Sedan *** Ready To Upgrade Your Ride Today? We Make It Fast & Easy!Call (or text) ☏ (604) 245-3009 Alpha Auto Access LTD. 5708 Kingsway Burnaby British Columbia, Burnaby, BC  V5H 2G6Or use the link belowto view more information!http://alphaauto.ca/cars/17458215Year : 2012Make : HondaModel : Civic SdnOdometer : 155,000\xa0Transmission : AutomaticExterior Color : BlackInterior Color : GraySeries : SedanDrivetrain :  FWDCondition : ExcellentVIN : 2HGFB2F42CH008922Stock ID : A11438Engine : 1.8L 140.0hpDescription of this Honda Civic Sdn 4dr Auto LX Sedan2012 Honda Civic 4dr Auto, 155000 km, Local, A/C, 4cyl, Black on Grey interior, Power windows, Power locks, AM/FM/CD, AUX, Bluetooth, Cruise control, Saefty inspected, Warranty availableOptional equipment of this Honda Civic Sdn 4dr Auto LX Sedan   Call (or Text)  (604) 245-3009 for quick answers to your questions about this Honda Civic Sdn 4dr Auto LX.\xa0 ALPHA AUTO ACCESS LTD \xa0 5708 KINGSWAY, Burnaby, V5H 2G6 Phone: (604) 336-9599 Website: www.alphaauto.ca \xa0 \xa0 Mechanical inspection. History reports available. \xa0 \xa0 -We buy your used car or truck. -Trades are welcome. -Use your car as part of your down payment. -We pay more than black book for your lease return vehicles. \xa0 Financing available for cars up to ten years old \xa0 WARRANTY AVAILABLE, TRADES WELCOME, Options may Vary, DL#30758, FOR MORE INFO Visit, www.alphaauto.ca or ****PLEASE CALL (604) 336-9599**** \xa0 DOCUMENTATION FEE $385 AND 12% SALES TAX TO BE APPLIED, \xa0***** Honda Civic Sdn 4dr Auto LX Sedan *****2013, 2014, 2015, 2012, 2011, 2010, 2009, Honda, Civic Sdn, Accord, CR-V, Civic, Civic Hybrid, [Model5]Disclaimer :  * Please Note All Inventory And Inventory Pricing Is Subject To Change Please See Dealer For Further Details * Ready To Upgrade Your Ride Today? We Make It Fast & Easy!Call (or text) ☏ (604) 245-3009 Alpha Auto Access LTD. 5708 Kingsway Burnaby British Columbia, Burnaby, BC  V5H 2G6Or use the link belowto view more information!http://alphaauto.ca/cars/17458215*Honda* *Accord* *CR-V* *Honda* Civic* Honda* *Civic Hybrid* *Automatic* *4dr Auto LX* *Honda* *Black* *Automatic* *Sedan* *1.8L 140.0hp* *FWD* *Alpha Auto Access LTD.* *Call us today at (604) 245-3009* *Honda Civic Sdn 4dr Auto LX Sedan FWD 1.8L 140.0hp* *Honda* *4dr Auto LX* *Honda Civic Sdn 4dr Auto LX Sedan FWD 1.8L 140.0hp**Honda* *Black* *Automatic* *Sedan* *1.8L 140.0hp* *FWD* *Call us today at (604) 245-3009* *Honda* *Black* *Automatic* *Alpha Auto Access LTD.* *Sedan* *1.8L 140.0hp* 2008 2007 2006 2005 2004 2003  '}
