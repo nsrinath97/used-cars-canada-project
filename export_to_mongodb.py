5da511b162fcaeb8a8effb934eab73bac807cbbb
Srinath Nanduri <67294707+nsrinath97@users.noreply.github.com>
2023-06-23 20:46:04 +0000

    last steps before creating the webpage

--- export_to_mongodb.py
+++ export_to_mongodb.py
@@ -2,21 +2,24 @@ from build_database import build_db_content
 import os
 from pymongo.mongo_client import MongoClient
 from pymongo.server_api import ServerApi
-from sample_posting import sample_post
 
+def connect_to_mongo_db():
 
-MONGO_DB_PASS = os.environ['MONGO_DB_PASS']
+    MONGO_DB_PASS = os.environ['MONGO_DB_PASS']
 
-uri = "mongodb+srv://nsrinath97:" + MONGO_DB_PASS + "@used-cars-vancouver.2tpj6or.mongodb.net/?retryWrites=true&w=majority"
-# Create a new client and connect to the server
-client = MongoClient(uri)
-# Send a ping to confirm a successful connection
-try:
-    client.admin.command('ping')
-    print("Pinged your deployment. You successfully connected to MongoDB!")
-except Exception as e:
-    print(e)
+    uri = "mongodb+srv://nsrinath97:" + MONGO_DB_PASS + "@used-cars-vancouver.2tpj6or.mongodb.net/?retryWrites=true&w=majority"
+    # Create a new client and connect to the server
+    client = MongoClient(uri)
+    # Send a ping to confirm a successful connection
+    try:
+        client.admin.command('ping')
+        print("Pinged your deployment. You successfully connected to MongoDB!")
+    except Exception as e:
+        print(e)
 
+    return client
+
+client = connect_to_mongo_db()
 
 db = client['used_cars']
 collection = db['posting_details']
