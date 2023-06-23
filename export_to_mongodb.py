9a3ba681160df94e54fa1f05f6325e2ff1b24340
Srinath Nanduri <67294707+nsrinath97@users.noreply.github.com>
2023-06-23 20:25:09 +0000

    successful pipeline from craigslist to mongodb

--- export_to_mongodb.py
+++ export_to_mongodb.py
@@ -1,16 +1,15 @@
 from build_database import build_db_content
 import os
-from sample_posting import sample_post
 from pymongo.mongo_client import MongoClient
 from pymongo.server_api import ServerApi
-
+from sample_posting import sample_post
 
 
 MONGO_DB_PASS = os.environ['MONGO_DB_PASS']
 
 uri = "mongodb+srv://nsrinath97:" + MONGO_DB_PASS + "@used-cars-vancouver.2tpj6or.mongodb.net/?retryWrites=true&w=majority"
 # Create a new client and connect to the server
-client = MongoClient(uri, server_api=ServerApi('1'))
+client = MongoClient(uri)
 # Send a ping to confirm a successful connection
 try:
     client.admin.command('ping')
@@ -22,10 +21,7 @@ except Exception as e:
 db = client['used_cars']
 collection = db['posting_details']
 
-# used_cars_database = build_db_content()
-# collection.insert_many(used_cars_database)
-
-used_cars_database = sample_post
-collection.insert_one(used_cars_database)
+used_cars_database = build_db_content()
+collection.insert_many(used_cars_database)
 
 client.close()
