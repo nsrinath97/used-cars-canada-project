7708f03bd465a89f063659ac557ee6a139278d4f
Srinath Nanduri <67294707+nsrinath97@users.noreply.github.com>
2023-07-19 00:01:01 +0000

    added requirements & run scraper before db connect

--- export_to_mongodb.py
+++ export_to_mongodb.py
@@ -1,12 +1,14 @@
 from build_database import build_db_content
 from connect_to_mongodb import connect_to_mongo_db
 
+
+used_cars_database = build_db_content()
+
 client = connect_to_mongo_db()
 
 db = client['used_cars']
 collection = db['posting_details']
 
-used_cars_database = build_db_content()
 collection.insert_many(used_cars_database)
 
 client.close()
